20b4e9e1b10a9050007ad9676f2fc00cdb3e976b
Johnathan Kelley <jkellz592@gmail.com>
2020-03-10 17:14:38 -0400

    Completed Project Grid.

--- js/app.js
+++ js/app.js
@@ -12,29 +12,44 @@ let test = `https://spreadsheets.google.com/feeds/list/1-fYybuYXtChuaZc83JI35Pp0
 fetch(source)
   .then( response => response.json() ) // this parses the data from string to object
   .then( data => {
-  console.log(data)
-  let projects = data.feed.entry.map( project => {
-    // console.log(project.gsx$title.$t)
-    return { 
-      title: project.gsx$title.$t,
-      image: project.gsx$image.$t,
-      description: project.gsx$description.$t,
-      url: project.gsx$url.$t
-    }
-  })
+    console.log('data', data)
+    // data.feed.entry is the array that stores our projects
+    // the projects are stored as objects
+    let projects = data.feed.entry.map( project => {
+      // console.log('project', project.gsx$title.$t)
+      return {
+        title: project.gsx$title.$t,
+        image: project.gsx$image.$t,
+        description: project.gsx$description.$t,
+        url: project.gsx$url.$t
+      }
+    })
   app(projects)
-}) // this provides us access to the parsed data
+})
+.catch( err => console.log('err', err))
 
-function app (projects) {
-  console.log(projects)
+function app(projects) {
+console.log('app - projects', projects)
+
   //the rest of the code goes here, like what we need to do with the data
+  function createMyProjects(){
+    for(let i =0; i < projects.length; i++){
+        let $article = $(`
+        <article id="3685" class="location-listing">
+           <a class="project-titles" href=${projects[i].url}>${projects[i].title}</a>
+        <div class="project-image">
+        <a href=${projects[i].url}>
+             <img width="100%" height="100%" src=${projects[i].image} alt=""></a>
+        </div>
+        </article>`)
+
+        $('#grid-container').append($article)
+    }
+  }
+  createMyProjects()
 }
 
 
 // let projects = [
 //    {title: 'title of project', image: 'image url', description: 'some desc', url: 'url to project'}
 //  ]
-
-// for(let i = 0; i < projects.length; i++) {
-//   console.log(projects)
-// }
